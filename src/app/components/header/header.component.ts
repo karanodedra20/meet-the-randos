35ed44f96317b4e320687387be32da1d9bd723e5
karanodedra20 <karanodedra81@gmail.com>
2025-10-28 16:47:08 +0100

    feat: implement dark mode feature

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,11 +7,12 @@ import {
 } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { GroupingStrategy } from '../../models/user-group.model';
+import { SwitchComponent } from '../switch/switch.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, SwitchComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -30,6 +31,44 @@ export class HeaderComponent {
   nationalityFilter = signal<string>('');
 
   readonly GroupingStrategy = GroupingStrategy;
+
+  darkMode = signal<boolean>(false);
+
+  constructor() {
+    const saved = localStorage.getItem('pref-theme');
+    if (saved === 'dark') {
+      this.enableDarkMode();
+    } else if (saved === 'light') {
+      this.disableDarkMode();
+    } else {
+      if (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      ) {
+        this.enableDarkMode(false);
+      }
+    }
+  }
+
+  toggleDarkMode(): void {
+    this.darkMode() ? this.disableDarkMode() : this.enableDarkMode();
+  }
+
+  onThemeToggle(checked: boolean): void {
+    checked ? this.enableDarkMode() : this.disableDarkMode();
+  }
+
+  private enableDarkMode(persist: boolean = true): void {
+    document.body.classList.add('dark-theme');
+    this.darkMode.set(true);
+    if (persist) localStorage.setItem('pref-theme', 'dark');
+  }
+
+  private disableDarkMode(persist: boolean = true): void {
+    document.body.classList.remove('dark-theme');
+    this.darkMode.set(false);
+    if (persist) localStorage.setItem('pref-theme', 'light');
+  }
 
   onSearchChange(): void {
     this.searchChange.emit(this.searchQuery());
